f34053d2c848753cae97f099c7d343e8de3ea042
Henryvboop <61257724+Henryvboop@users.noreply.github.com>
2020-02-20 12:04:25 +1100

    Add files via upload

---
+++ HowAreYou.py
@@ -0,0 +1,19 @@
+HowTheyAre=input('How are you')
+if HowTheyAre =="Good"or HowTheyAre =="good":
+    print('Great')
+if HowTheyAre =="completely g fam":
+    print('Ima computer u noob')
+if HowTheyAre =="ok"or HowTheyAre =="OK"or HowTheyAre =="Okay"or HowTheyAre =="okay":
+    print ('I hope your day gets better')
+if HowTheyAre =="Not so good" or HowTheyAre == "Not the best":
+    print ('I hope your day gets better')
+NumberTheyPicked=input('pick a number; 1, 2, or 3')
+if NumberTheyPicked =="1":
+    print ('That is the value of 2-1')
+if NumberTheyPicked =="2":
+    print ('That is the value of 3-1 and 1+1')
+if NumberTheyPicked =="3":
+    print ('That is the value of 4-1 and 2+1 and 2X2-1')
+AnythingElse=input ('Anything else?')
+if AnythingElse == "thank you"or AnythingElse == "Thank you" or  AnythingElse =="thanks" or  AnythingElse =="Thanks" or AnythingElse == "Thankyou" or AnythingElse == "thankyou":
+    print ("You're welcome")
